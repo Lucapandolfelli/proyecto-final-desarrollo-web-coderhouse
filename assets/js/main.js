1fd2f49809d2e7b70d7de88b854dc280db5ac87d
Lucapandolfelli <lucapandolfelli2002@gmail.com>
2022-05-07 11:20:59 -0300

    Arreglando animaciones y otros elementos

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,16 @@ let menu = document.getElementById('menu');
 
 openMenu.addEventListener('click', () =>{
     menu.style.display = 'flex';
+    menu.classList.remove('move-out');
+    menu.classList.add('move-in');
     openMenu.style.display = 'none';
     closeMenu.style.display = 'block';
 });
 
 closeMenu.addEventListener('click', () =>{
-    menu.style.display = 'none';
+    /* menu.style.display = 'none'; */
+    menu.classList.remove('move-in');
+    menu.classList.add('move-out');
     openMenu.style.display = 'block';
     closeMenu.style.display = 'none';
 });
