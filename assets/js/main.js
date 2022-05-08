8b6389729bd219528e4e94fde328e8ace4f396dc
Lucapandolfelli <lucapandolfelli2002@gmail.com>
2022-05-07 21:03:43 -0300

    Añadiendo animación al botón del menu del navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
-let openMenu = document.getElementById('btn-toggle');
-let closeMenu = document.getElementById('btn-toggle-close');
+let toggle = document.getElementById('btn-toggle');
 let menu = document.getElementById('menu');
 
-openMenu.addEventListener('click', () =>{
-    menu.style.display = 'flex';
-    menu.classList.remove('move-out');
-    menu.classList.add('move-in');
-    openMenu.style.display = 'none';
-    closeMenu.style.display = 'block';
-});
+toggle.addEventListener('click', () =>{
 
-closeMenu.addEventListener('click', () =>{
-    /* menu.style.display = 'none'; */
-    menu.classList.remove('move-in');
-    menu.classList.add('move-out');
-    openMenu.style.display = 'block';
-    closeMenu.style.display = 'none';
+    if(toggle.classList.contains('close')){
+        menu.classList.remove('move-in');
+        menu.classList.add('move-out');
+        toggle.classList.remove('close');
+    }else{
+        toggle.classList.add('close');
+        menu.style.display = 'flex';
+        menu.classList.remove('move-out');
+        menu.classList.add('move-in');
+    }
+    
 });
